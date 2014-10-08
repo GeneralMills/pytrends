a2259709d9b289a96183ee6c07997961f71f1b0c
dreyco676 <dreyco676@gmail.com>
2014-10-08 16:22:34 -0500

    style changes

--- download.py
+++ download.py
@@ -2,15 +2,15 @@ from pyGTrends import pyGTrends
 import time
 from random import randint
 
-google_username = 'an_account@gmail.com'
-google_password = 'paasword'
+google_username = "an_account@gmail.com"
+google_password = "password"
 
-def getGTData( trendName= "Unknown", path='C:/', keywords = "Unknown", date="all", topicFg="False" ) :
+def getGTData(trendName='Unknown', path='C:/', keywords='Unknown', date='all', topicFg='False') :
     
-    connector = pyGTrends( google_username, google_password )
-    connector.download_report(keywords=keywords , trendName = trendName, date = date, topicFg=topicFg)
+    connector = pyGTrends(google_username, google_password)
+    connector.download_report(keywords=keywords , trendName=trendName, date=date, topicFg=topicFg)
     time.sleep(randint(5,10))  # Delay for a random amount of seconds (avoid bot detection)
 
     data = connector.csv(path, trendName)
-    print("File saved: %s " % ( trendName.replace(" ","_") + '.csv' ))
+    print("File saved: %s " % (trendName.replace(" ","_")+'.csv'))
 
