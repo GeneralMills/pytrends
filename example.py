bb015292d15af397685d3e0f7b446a78d7c8e79b
dreyco676 <dreyco676@gmail.com>
2014-10-08 22:00:53 -0500

    made file into an example

---
+++ example.py
@@ -0,0 +1,18 @@
+from pyGTrends import pyGTrends
+import time
+from random import randint
+
+google_username = "an_account@gmail.com"
+google_password = "password"
+path = ""
+
+#connect to Google
+connector = pyGTrends(google_username, google_password)
+
+#make request
+connector.request_report("Pizza", "Pizza")
+
+#download file
+connector.csv(path, "pizza")
+
+
