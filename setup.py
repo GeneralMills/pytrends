2232414d7952a483a06b94651148c1d2a49e9ba7
Burton DeWilde <burtondewilde@gmail.com>
2015-08-11 15:48:28 -0400

    added .gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import io
 import os.path
 from setuptools import setup
 
-here = os.path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -20,7 +20,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'  ## TODO
+        'License :: OSI Approved :: MIT License'  # TODO
         ],
     keywords='google trends pseudo-API',
     packages=['pytrends'],
