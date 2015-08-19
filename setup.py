b2c6ee048ee1aa6a75e98c90be23a59ec211f3a6
John Hogue <dreyco676@gmail.com>
2015-08-19 09:18:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(os.path.join(dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pytrends',
-    version='1.0.4',
+    version='1.0.5',
     description='Pseudo API for Google Trends',
     long_description=long_description,
     url='https://github.com/dreyco676/pytrends',
