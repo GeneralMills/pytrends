0f6613ae4f7013bf539b7aa52784b394b1414459
Burton DeWilde <burtondewilde@gmail.com>
2015-08-24 13:40:18 -0400

    added arrow dependency for easier date handling
    preparing to add in daily granularity parameter for searches, which
    adds a LOT of complexity, but is a necessary feature for certain tasks

--- pytrends/pytrends.py
+++ pytrends/pytrends.py
@@ -4,6 +4,8 @@ from datetime import date, datetime
 import io
 import json
 
+import arrow
+
 from .compat import quote, string_type
 from .connect import GoogleConnection
 from .parse import parse_data
@@ -22,7 +24,7 @@ class GoogleTrends(object):
         self.connection = GoogleConnection(username, password)
 
     def query(self, terms, is_topic=False,
-              start_date=None, end_date=None,
+              start_date=None, end_date=None, granularity='auto',
               category_filter=None, geo_filter=None, search_filter=None):
         """
         Query Google Trends for `terms`, optionally filtering by date,
@@ -40,18 +42,26 @@ class GoogleTrends(object):
             if False (default), all terms are assumed to be "search terms",
             so a same-length list of False values is auto-generated;
             if True, a same-length list of True values is auto-generated
-        start_date : str or `datetime.date`, optional
+        start_date : str, `datetime.datetime`, or `datetime.date`, optional
             if None (default), effective value is 2004-01-01 (or 2008-01-01);
             if str, must be in ISO standard format: YYYY-MM-DD;
             Note: day doesn't matter, only year and month
-        end_date : str or `datetime.date`, optional
+        end_date : str, `datetime.datetime`, or `datetime.date`, optional
             if None (default), effective value is today's date;
             if str, must be in ISO standard format: YYYY-MM-DD;
             Note: day doesn't matter, only year and month
+        granularity : str {'auto', 'daily'}, optional
+            if 'auto' (default), Google Trends will automatically determine the
+            granularity of search volume over time (see Notes below);
+            if 'daily', a series of 3-month queries are concatenated together,
+            and search volume values between 3-month spans are scaled to match
+            based on shared values for overlapping months
         category_filter : str, optional
-            if None (default), results from 'all categories' are returned
+            if None (default), results from 'all categories' are returned;
+            otherwise, manually extract valid values from the web GUI's URLs
         geo_filter : str, optional
             if None (default), results from 'worldwide' searches are returned
+            otherwise, manually extract valid values from the web GUI's URLs
         search_filter : str {'web', 'images', 'news', 'froogle', 'youtube'}, optional
             if None (default), only results from 'web' searches are returned
 
@@ -64,24 +74,7 @@ class GoogleTrends(object):
         and some don't return enough data, the csv will automatically exclude them.
         """
         query_param = self._process_query_terms(terms, is_topic)
-        if search_filter:
-            if search_filter not in self.search_filters:
-                msg = '`search_filter` {0} not valid; options are {1}'.format(
-                    search_filter, self.search_filters)
-                raise ValueError(msg)
-            else:
-                gprop_param = 'gprop={}'.format(search_filter)
-        else:
-            gprop_param = None
-        start_date, end_date = self._process_date_filter(
-            start_date, end_date, search_filter)
-        if start_date and end_date:
-            date_param = '{0} {1}m'.format(
-                start_date.strftime('%m/%Y'),
-                1 + 12*(end_date.year-start_date.year) + (end_date.month-start_date.month))
-            date_param = 'date={}'.format(quote(date_param, safe=''))
-        else:
-            date_param = None
+        gprop_param = self._process_search_filter(search_filter)
         if category_filter:
             cat_param = 'cat={}'.format(category_filter)
         else:
@@ -90,11 +83,23 @@ class GoogleTrends(object):
             geo_param = 'geo={}'.format(geo_filter)
         else:
             geo_param = None
-
-        # default params, not to be changed
+        # these three are default params -- not to be changed!
         cmpt_param = 'cmpt=q'
         content_param = 'content=1'
         export_param = 'export=1'
+
+        start_date, end_date = self._process_date_filter(start_date, end_date,
+                                                         search_filter)
+        if start_date and end_date:
+            if granularity == 'auto':
+                date_param = '{0} {1}m'.format(
+                    start_date.format('MM/YYYY'),
+                    1 + 12*(end_date.year-start_date.year) + (end_date.month-start_date.month))
+                date_param = 'date={}'.format(quote(date_param, safe=''))
+            else:
+                raise NotImplementedError('still working on daily granularity')
+        else:
+            date_param = None
 
         params = [query_param, cat_param, date_param, gprop_param, geo_param,
                   cmpt_param, content_param, export_param]
@@ -120,26 +125,37 @@ class GoogleTrends(object):
                        for term, topic in zip(terms, is_topic)))
         return query_param
 
+    def _process_gprop_param(self, search_filter):
+        if search_filter:
+            if search_filter not in self.search_filters:
+                msg = '`search_filter` {0} not valid; options are {1}'.format(
+                    search_filter, self.search_filters)
+                raise ValueError(msg)
+            else:
+                gprop_param = 'gprop={}'.format(search_filter)
+        else:
+            gprop_param = None
+        return gprop_param
+
     def _process_date_filter(self, start_date, end_date, search_filter=None):
         if not search_filter or search_filter == 'web':
-            min_start_date = date(year=2004, month=1, day=1)
+            min_start_date = arrow.get('2004-01-01')
         else:
-            min_start_date = date(year=2008, month=1, day=1)
+            min_start_date = arrow.get('2008-01-01')
+        max_end_date = arrow.now()
         if start_date:
-            if not isinstance(start_date, date):
-                start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+            start_date = arrow.get(start_date)
             if start_date < min_start_date:
-                msg = 'Earliest available Google Trends data is {}'.format(
+                msg = 'Earliest available Google Trends data is for {}'.format(
                     min_start_date)
                 raise ValueError(msg)
             if not end_date:
-                end_date = date.today()
+                end_date = max_end_date
         if end_date:
-            if not isinstance(end_date, date):
-                end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-            if end_date > date.today():
-                msg = 'Latest available Google Trends data is {} (today)'.format(
-                    date.today())
+            end_date = arrow.get(end_date)
+            if end_date > max_end_date:
+                msg = 'Last available Google Trends data is for {} (today)'.format(
+                    max_end_date)
                 raise ValueError(msg)
             if not start_date:
                 start_date = min_start_date
