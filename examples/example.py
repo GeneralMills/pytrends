20346e445b94162060a9d4396517a7c9adc1b868
dreyco676 <dreyco676@gmail.com>
2015-09-14 11:04:41 -0500

    replaced example

--- examples/example.py
+++ examples/example.py
@@ -1,1 +1,24 @@
+from pytrends.pyGTrends import pyGTrends
+import time
+from random import randint
 
+google_username = "an_email@gmail.com"
+google_password = "password"
+path = ""
+
+# connect to Google
+connector = pyGTrends(google_username, google_password)
+
+# make request
+connector.request_report("Pizza")
+
+# wait a random amount of time between requests to avoid bot detection
+time.sleep(randint(5,10))
+
+# download file
+connector.save_csv(path, "pizza")
+
+# get suggestions for keywords
+keyword = "milk substitute"
+data = connector.get_suggestions(keyword)
+print(data)
