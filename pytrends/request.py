61befed406f67a168ba326bf2e9e606a44e9a9d3
John Hogue <dreyco676@gmail.com>
2017-01-24 23:24:54 -0600

    reverting changes

--- pytrends/request.py
+++ pytrends/request.py
@@ -55,8 +55,7 @@ class TrendReq(object):
     def trend(self, payload, return_type=None):
         payload['cid'] = 'TIMESERIES_GRAPH_0'
         payload['export'] = 3
-        req_url = "https://www.google.com/trends/api/widgetdata/multiline?hl=en-US&tz=360&req=%7B%22time%22:%222012-01-24+2017-01-24%22,%22resolution%22:%22WEEK%22,%22locale%22:%22en-US%22,%22comparisonItem%22:%5B%7B%22geo%22:%7B%7D,%22complexKeywordsRestriction%22:%7B%22keyword%22:%5B%7B%22type%22:%22ENTITY%22,%22value%22:%22%2Fm%2F09cfq%22%7D%5D%7D%7D%5D,%22requestOptions%22:%7B%22property%22:%22%22,%22backend%22:%22IZG%22,%22category%22:0%7D%7D&token=APP6_UEAAAAAWIkz8jJdwRodB4PjYPZqIeDRpPrBqzB0&tz=360"
-        print(self.ses.cookies.get_dict())
+        req_url = "http://www.google.com/trends/fetchComponent"
         req = self.ses.get(req_url, params=payload)
         try:
             if self.google_rl in req.text:
