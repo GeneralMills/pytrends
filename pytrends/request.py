4c7ad699097cabf024eed86f07b673582388f927
Shoya Yoshida <sy519@georgetown.edu>
2018-05-26 18:13:26 -0400

    move get_historical to TrendReq class

--- pytrends/request.py
+++ pytrends/request.py
@@ -6,6 +6,9 @@ import sys
 import pandas as pd
 import requests
 
+from datetime import datetime, timedelta
+import time
+
 from pytrends import exceptions
 
 if sys.version_info[0] == 2:  # Python 2
@@ -370,3 +373,44 @@ class TrendReq(object):
         )
         return req_json
 
+    def get_historical_interest(self, keywords, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2018, month_end=2, day_end=1, hour_end= 0, cat=0, geo='', gprop='', sleep=0):
+        """Gets historical hourly data for interest by chunking requests to 1 week at a time (which is what Google allows)"""
+
+        # construct datetime obejcts - raises ValueError if invalid parameters       
+        start_date = datetime(year_start, month_start, day_start, hour_start)
+        end_date = datetime(year_end, month_end, day_end, hour_end)
+
+        # the timeframe has to be in 1 week intervals or Google will reject it 
+        delta = timedelta(days=7)
+
+        df = pd.DataFrame()
+
+        date_iterator = start_date
+        date_iterator += delta 
+
+        while True:
+            if (date_iterator > end_date):
+                # has retrieved all of the data 
+                break
+
+            # format date to comply with API call 
+            start_date_str = start_date.strftime('%Y-%m-%dT%H')
+            date_iterator_str = date_iterator.strftime('%Y-%m-%dT%H')
+            tf = start_date_str + ' ' + date_iterator_str
+
+            try: 
+                self.build_payload(keywords,cat, tf, geo, gprop)
+                week_df = self.interest_over_time()
+                df = df.append(week_df)
+            except Exception as e:
+                print(e)
+                pass
+
+            start_date += delta
+            date_iterator += delta 
+
+            # just in case you are rate-limited by Google. Recommended is 60 if you are. 
+            if sleep > 0:
+                time.sleep(sleep)
+
+        return df 
