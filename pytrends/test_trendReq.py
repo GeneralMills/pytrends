8000997b1946b8d196bfc0ecb22b1dd785a6d4ed
Max Ghenis <mghenis@gmail.com>
2020-06-08 20:39:51 -0700

    Add US tests to interest_by_subregion

--- pytrends/test_trendReq.py
+++ pytrends/test_trendReq.py
@@ -49,6 +49,26 @@ class TestTrendReq(TestCase):
         pytrend.build_payload(kw_list=['pizza', 'bagel'])
         self.assertIsNotNone(pytrend.interest_by_region(resolution='CITY'))
 
+    def test_interest_by_region_us(self):
+        pytrend = TrendReq()
+        pytrend.build_payload(kw_list=['pizza', 'bagel'], geo='US')
+        self.assertIsNotNone(pytrend.interest_by_region())
+
+    def test_interest_by_dma_us(self):
+        pytrend = TrendReq()
+        pytrend.build_payload(kw_list=['pizza', 'bagel'], geo='US')
+        self.assertIsNotNone(pytrend.interest_by_region(resolution='DMA'))
+
+    def test_interest_by_subregion_us(self):
+        pytrend = TrendReq()
+        pytrend.build_payload(kw_list=['pizza', 'bagel'], geo='US')
+        self.assertIsNotNone(pytrend.interest_by_region(resolution='REGION'))
+
+    def test_interest_by_city_us(self):
+        pytrend = TrendReq()
+        pytrend.build_payload(kw_list=['pizza', 'bagel'], geo='US')
+        self.assertIsNotNone(pytrend.interest_by_region(resolution='CITY'))
+
     def test_interest_by_dma_ca(self):
         # DMA don't exist in Canada, so this should return an error.
         pytrend = TrendReq()
